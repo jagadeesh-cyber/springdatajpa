478a2e9167b1c5c7acc25b5ff9750a730a94176f
jagadeesh-cyber <58551878+jagadeesh-cyber@users.noreply.github.com>
2021-02-03 20:50:22 +0530

    added comment on controller class

--- src/main/java/com/infy/controller/ProductRestController.java
+++ src/main/java/com/infy/controller/ProductRestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.infy.entities.Product;
 import com.infy.repos.ProductRepo;
-//
+//controller class
 @RestController
 public class ProductRestController {
 	@Autowired
